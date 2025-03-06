a6eb0e174a324632163bc2b6cf7dbf58e645b175
Raul <raulog1989@gmail.com>
2025-03-06 08:55:03 +0100

    Initialise test work * Add assertion for job being dispatched * Add skeleton test about request failing controller validation * Refactor test to make it DRY and use attributes to minimise test name length

--- tests/Feature/CsvUploadControllerTest.php
+++ tests/Feature/CsvUploadControllerTest.php
@@ -1,25 +1,41 @@
 <?php
 
-// tests/Feature/CsvUploadControllerTest.php
 namespace Tests\Feature;
 
+use App\Jobs\ProcessCsvUploadJob;
 use App\Models\CsvUpload;
 use App\Models\User;
+use Illuminate\Bus\Dispatcher;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
+use Mockery\MockInterface;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class CsvUploadControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
-    // Test the CSV upload route
-    public function test_it_can_upload_a_csv_file()
-    {
-        $user = User::factory()->create(); // Create a user for testing
+    private User $user;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $user = User::factory()->create();
+        $this->user = $user;
         $this->actingAs($user);
+    }
+
+    #[Test] public function invalidates_uploads() {
+        // Assert that invalid uploads such as missing file are not accepted
+    }
+
+    #[Test] public function dispatches_csv_upload_job()
+    {
+        $this->mock(Dispatcher::class, function (MockInterface $mock) {
+            $mock->shouldReceive('dispatch')->once()->with(ProcessCsvUploadJob::class);
+        });
+
         $file = UploadedFile::fake()->create('addresses.csv', 100, 'text/csv');
         $response = $this->post(route('csv.upload'), [
             'file' => $file,
@@ -28,13 +44,7 @@ class CsvUploadControllerTest extends TestCase
         $response->assertStatus(200);
         $this->assertDatabaseHas('csv_uploads', [
             'file_name' => 'addresses.csv',
-            'uploaded_by' => $user->id,
+            'uploaded_by' => $this->user->id,
         ]);
     }
-
-
-
-
-
 }
-
